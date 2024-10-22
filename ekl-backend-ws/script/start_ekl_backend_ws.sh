137549d1536b074d6adc927e7087bcdf64fef825
Qiao Han <timhanq@hotmail.com>
2024-10-22 22:03:04 +0200

    upd Max Heap Size

--- ekl-backend-ws/script/start_ekl_backend_ws.sh
+++ ekl-backend-ws/script/start_ekl_backend_ws.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "start ekl-backend-ws with spring profiles $EKL_BACKEND_WS_PROFILES"
-java -Dspring.profiles.active=$EKL_BACKEND_WS_PROFILES -jar /usr/local/bin/ekl-backend-ws.jar
+java -Dspring.profiles.active=$EKL_BACKEND_WS_PROFILES -XX:+AlwaysActAsServerClassMachine -XX:+UseParallelGC -Xmx1000M -XX:MaxDirectMemorySize=40M -XX:MaxMetaspaceSize=255M -jar /usr/local/bin/ekl-backend-ws.jar
